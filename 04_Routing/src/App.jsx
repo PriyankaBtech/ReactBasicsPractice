bf31df3e5b572f18e193df7ddf71133bc2f916f3
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-11-27 16:34:20 +0530

    Routing using react-router-rom

---
+++ 04_Routing/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
